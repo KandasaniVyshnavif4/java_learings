4e9a888f64d8f5ecc339223af2cac0b3f370ba14
Hari-chandraprasad <govindaraodannana5@gmail.com>
2024-08-04 22:02:20 +0530

    Add files via upload

--- Linear.java
+++ Linear.java
@@ -1,48 +1,41 @@
-import java.util.*;
-class Linear
-{
+import java.util.Scanner;
+
+class Linear {
     static int key;
     static int n;
     
-    
-    public void check(int n,int ar[],int key)
-    {
-        int count = 2;
-        for(int i=0;i<n;i++)
-        {
-            if(ar[i] == key)
-            {
-                count = 0;
+    public void check(int n, int ar[], int key) {
+        boolean found = false;
+        for(int i = 0; i < n; i++) {
+            if(ar[i] == key) {
+                found = true;
                 break;
             }
-            else
-            {
-              count += 1;
-            }
         }
-        if (count == 0)
-        {
-            System.out.println("yes found ");
-        }
-        else{
+        if (found) {
+            System.out.println("yes found");
+        } else {
             System.out.println("not found");
         }
     }
-    public static void main(String[] args)
-    {
-       Scanner sc = new Scanner(System.in);
-    //    Linear l1= new Linear();
-       n =sc.nextInt();
-    Linear l1 = new Linear();
-    System.out.println("enter the key");
-        key =sc.nextInt();
-       int ar[] =new int[n];
-       System.out.println("enter array elements");
-       for(int i =0;i<n;i++)
-       {
-          ar[i] = sc.nextInt();
-       }
-       l1.check(n,ar,key);
-       sc.close();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements:");
+        n = sc.nextInt();
+        Linear l1 = new Linear();
+
+        System.out.println("Enter the key to search:");
+        key = sc.nextInt();
+
+        int ar[] = new int[n];
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++) {
+            ar[i] = sc.nextInt();
+        }
+
+        l1.check(n, ar, key);
+        sc.close();
     }
 }
