7262de4794deda9eb6755306d2f42f5eaf3c70d4
KandasaniVyshnavif4 <153075546+KandasaniVyshnavif4@users.noreply.github.com>
2024-08-19 20:57:02 +0530

    Add files via upload
    first one basic calculator

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+//today we are learning inheretence we use ""extends"" keyword in order to do that the following files
+ //will explain the multilevel inheretence 
+ //1)Calculator
+ //2) Advcal
+ //3) Demo
+
+public class Calculator 
+{
+    public void  add(int a,int b)
+    {
+        System.out.println("a+b is" + (a+b));
+    }
+    public void sub(int a,int b)
+    {
+        System.out.println("a-b is" + (a-b));
+    }
+    public void multi(int a, int b)
+    {
+        System.out.println("a*b is" + (a*b));
+    }
+    public void divi(int a,int b)
+    {
+        System.out.println("a/b is" + (a/b));
+    }
+  
+}
+
