99cfa9dcaa4293b600c9d836a5f83d7a49807c22
KandasaniVyshnavif4 <153075546+KandasaniVyshnavif4@users.noreply.github.com>
2024-08-19 21:01:32 +0530

    Add files via upload
    It contains main method of Calculator and Advcal

---
+++ Demo.java
@@ -0,0 +1,15 @@
+class Demo
+{
+    public static void main(String[] args)
+    {
+        Advcal c1 = new Advcal ();
+        c1.add(2,3);
+       c1.sub(4,3);
+       c1.multi(2, 1);
+       c1.divi(4,2);
+    //    Advcal c2 = new Advcal ();
+       c1.power(2,3);
+
+      
+    }
+}
