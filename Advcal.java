eee1f9422601e91b5f6b43cda007972434df8cab
KandasaniVyshnavif4 <153075546+KandasaniVyshnavif4@users.noreply.github.com>
2024-08-19 21:00:19 +0530

    Add files via upload
    extends to Calculator

---
+++ Advcal.java
@@ -0,0 +1,12 @@
+public class Advcal extends Calculator 
+{
+  public void power(int a,int b)
+  {
+    double r=  Math.pow(a,b);
+    System.out.println(r+"is power");
+  }   
+  public int moduler(int a, int b)
+  {
+    return a%b;
+  }
+}
